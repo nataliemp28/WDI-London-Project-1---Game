e21a5064f74ed35d7470cb87b5db726deedeadf4
Natalie Pateman <nataliempateman@gmail.com>
2016-12-26 14:20:39 +0000

    fixing game styling

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 $(function() {
 
-  let categoryOne = window.categoryOne;
-  let categoryTwo = window.categoryTwo;
-  let categoryThree = window.categoryThree;
-  let activeWord;
-  let score = 0;
-  let highScore = [];
-  let updatedHighScore = 0;
+  var categoryOne = window.categoryOne;
+  var categoryTwo = window.categoryTwo;
+  var categoryThree = window.categoryThree;
+  var activeWord;
+  var score = 0;
+  var highScore = [];
+  var updatedHighScore = 0;
 
   //shuffle function
-let shuffleWord = word => {
-  // function shuffleWord(word)
+
+  function shuffleWord(word) {
     var j, x, i;
     var wordArray = word.split('');
     for (i = wordArray.length; i; i--) {
@@ -25,8 +25,8 @@ let shuffleWord = word => {
   //count down timer function
 
   $('#startButton').on('click', function() {
-    soundtrack.src ="Scrambler.wav";
-    soundtrack.play();
+    soundtrack.src = 'Scrambler.wav';
+    soundtrack.play(true,true);
   });
   $('#startButton').on('click', counter)
   function counter() {
@@ -35,20 +35,35 @@ let shuffleWord = word => {
     function timer() {
       count=count-1;
       if (count <=5) {
-        $("#timer").css("color", "red");
+        $('#timer').css('color', 'red');
       }
       if (count === 0) {
         clearInterval(counter);
         soundtrack.pause(true,true);
         failSound.pause(true,true);
-        alert("You're out of time");
+        alert('You are out of time');
         return;
       }
-      $("#timer").text(count + " Seconds");
+      $('#timer').text(count + ' Seconds');
     }
-  };
+  }
+  //resets the scrambled word and score so that player can play again. restarts soundtrack.
 
-  //function to run onclick of selctor button one. Calls the shuffle word function and applies it to
+  $('#reset').on('click', function() {
+    $('#scrambled').empty();
+    score = 0;
+    $('#playerScore').text('0');
+    $('#timer').css('color', 'black');
+    $('#currentPlayerWord').val('');
+    soundtrack.play(true,true);
+    counter();
+
+    highScore.push(score);
+    var currentHighScore = Math.max.apply(Math,highScore);
+    $('#highScore').text(currentHighScore);
+  });
+
+  //function to run onclick of selector button one. Calls the shuffle word function and applies it to
   //the scrambled word that will be displayed in the display box.
 
   $('#selectorButtonOne').on('click', function() {
@@ -69,37 +84,23 @@ let shuffleWord = word => {
     if (playerWord === activeWord) {
       score += playerWord.length;
       $('#playerScore').text(score);
-    }
-    else {
+    } else {
       $('#playerScore').text(score -=1);
       soundtrack.pause(true,true);
-      failSound.src ="fail-buzzer.wav";
+      failSound.src = 'fail-buzzer.wav';
       failSound.play();
     }
     soundtrack.play(true,true);
 
 //push the score to the highscore array. current high score will then check to make sure high score is up to date.
 
-    $('#currentPlayerWord').val("");
+    $('#currentPlayerWord').val('');
     highScore.push(score);
     var currentHighScore = Math.max.apply(Math,highScore);
     $('#highScore').text(currentHighScore);
   });
 
-  //resets the scrambled word and score so that player can play again. restarts soundtrack.
 
-  $('#reset').on('click', function() {
-    $('#scrambled').empty();
-    score = 0;
-    $('#playerScore').text('0');
-    $('#currentPlayerWord').val("");
-    soundtrack.play(true, true);
-    counter();
-
-    highScore.push(score);
-    var currentHighScore = Math.max.apply(Math,highScore);
-    $('#highScore').text(currentHighScore);
-  });
 
   //  button two and button three. Very repetitive but basically do the same as button one above.
 
